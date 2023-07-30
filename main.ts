aa8e10441914403c54ccc1095443e312292f7fb5
Michael Muniz <mcmuniz218@gmail.com>
2023-07-30 18:32:33 -0400

    init

--- main.ts
+++ main.ts
@@ -1,86 +1,97 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Vault, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface PipedreamToObsidianSettings {
+	apiEndpoint: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PipedreamToObsidianSettings = {
+	apiEndpoint: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PipedreamToObsidian extends Plugin {
+	settings: PipedreamToObsidianSettings;
+	urlRegex: RegExp = new RegExp('^(http|https)://', 'i');
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        await this.loadSettings();
+        
+        await this.getTodoistTasks();
+        new Notice('Synced Tasks');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+			id: 'make-pipedream-call',
+			name: 'Sync Tasks',
+            callback: async () => {
+                await this.getTodoistTasks();
+                new Notice('Synced Tasks');
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new PipedreamToObsidianSettingsTab(this.app, this));
 	}
 
 	onunload() {
 
-	}
+    }
+
+    getTodoistTasks = async () => {
+        // Validate settings.apiEndpoint is a valid http/https url with regex
+        if (!this.urlRegex.test(this.settings.apiEndpoint)) {
+            new Notice('Please configure the plugin settings before making a call.');
+            return;
+        }
+
+        const headers = new Headers()
+        headers.append("Content-Type", "application/json")
+
+        const body = {
+            "test": "event"
+        }
+
+        const options: RequestInit = {
+            method: "POST",
+            headers,
+            mode: "cors",
+            body: JSON.stringify(body),
+        }
+        // Make a call to the Pipedream API
+        const response = await fetch(this.settings.apiEndpoint, options)
+
+        if (!response.ok) {
+            new Notice(`Error: ${response.status} ${response.statusText}`);
+            return;
+        }
+        const responseData = await response.json();
+
+        const todoistTasks: Task[] = Object.values(responseData);
+
+        for (const task of todoistTasks) {
+            await this.todoistTaskToNote (task);
+        }
+    }
+    
+    todoistTaskToNote = async (task: Task) => {
+        // Convert the task into markdown with extra props as frontmatter
+        const taskMarkdown = 
+`---
+id: ${task.id}
+labels: ${task.labels}
+priority: ${task.priority}
+${task.assigneeId ? `created: ${task.assigneeId}` : ''}
+---
+${task.content}
+${task.description}
+`;
+        // Parse the title to remove *"\/<>:|? that wouldn't work as a file name
+        const title = task.content.replace(/[*"\\/<>:|?]/g, '');
+        // Create a new file in the vault with the task content
+        const newFile = await this.app.vault.create(`${title}.md`, taskMarkdown);
+
+    }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -107,10 +118,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class PipedreamToObsidianSettingsTab extends PluginSettingTab {
+	plugin: PipedreamToObsidian;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PipedreamToObsidian) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -125,10 +136,208 @@ class SampleSettingTab extends PluginSettingTab {
 			.setDesc('It\'s a secret')
 			.addText(text => text
 				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setValue(this.plugin.settings.apiEndpoint)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.apiEndpoint = value;
 					await this.plugin.saveSettings();
 				}));
 	}
+}
+
+import {
+    Boolean,
+    Number as NumberRunType,
+    String,
+    Array,
+    Record,
+    Static,
+    Partial,
+    Literal,
+    Union,
+    Null,
+} from 'runtypes'
+
+export const Int = NumberRunType.withConstraint(
+    (n) => Number.isInteger(n) || `${n} is not a valid entity id. Should be a string`,
+)
+
+export type TodoistEntity = {
+    id: string
+}
+
+export type OrderedEntity = TodoistEntity & {
+    order: number
+}
+
+export type EntityInHierarchy = OrderedEntity & {
+    parentId?: string | null
+}
+
+export const DueDate = Record({
+    isRecurring: Boolean,
+    string: String,
+    date: String,
+}).And(
+    Partial({
+        datetime: String.Or(Null),
+        timezone: String.Or(Null),
+    }),
+)
+
+export type DueDate = Static<typeof DueDate>
+
+export const Task = Record({
+    id: String,
+    order: Int,
+    content: String,
+    description: String,
+    projectId: String,
+    isCompleted: Boolean,
+    labels: Array(String),
+    priority: Int,
+    commentCount: Int,
+    createdAt: String,
+    url: String,
+    creatorId: String,
+}).And(
+    Partial({
+        due: DueDate.Or(Null),
+        assigneeId: String.Or(Null),
+        assignerId: String.Or(Null),
+        parentId: String.Or(Null),
+        sectionId: String.Or(Null),
+    }),
+)
+
+export type Task = Static<typeof Task>
+
+export const Project = Record({
+    id: String,
+    name: String,
+    color: String,
+    commentCount: Int,
+    isShared: Boolean,
+    isFavorite: Boolean,
+    url: String,
+    isInboxProject: Boolean,
+    isTeamInbox: Boolean,
+    order: Int,
+    viewStyle: String,
+}).And(
+    Partial({
+        parentId: String.Or(Null),
+    }),
+)
+
+export type Project = Static<typeof Project>
+
+export const Section = Record({
+    id: String,
+    order: Int,
+    name: String,
+    projectId: String,
+})
+
+export type Section = Static<typeof Section>
+
+export const Label = Record({
+    id: String,
+    order: Int,
+    name: String,
+    color: String,
+    isFavorite: Boolean,
+})
+
+export type Label = Static<typeof Label>
+
+export const Attachment = Record({
+    resourceType: String,
+}).And(
+    Partial({
+        fileName: String.Or(Null),
+        fileSize: Int.Or(Null),
+        fileType: String.Or(Null),
+        fileUrl: String.Or(Null),
+        fileDuration: Int.Or(Null),
+        uploadState: Union(Literal('pending'), Literal('completed')).Or(Null),
+        image: String.Or(Null),
+        imageWidth: Int.Or(Null),
+        imageHeight: Int.Or(Null),
+        url: String.Or(Null),
+        title: String.Or(Null),
+    }),
+)
+
+export type Attachment = Static<typeof Attachment>
+
+export const Comment = Record({
+    id: String,
+    content: String,
+    postedAt: String,
+}).And(
+    Partial({
+        taskId: String.Or(Null),
+        projectId: String.Or(Null),
+        attachment: Attachment.Or(Null),
+    }),
+)
+
+export type Comment = Static<typeof Comment>
+
+export const User = Record({
+    id: String,
+    name: String,
+    email: String,
+})
+
+export type User = Static<typeof User>
+
+export type Color = {
+    /**
+     * @deprecated No longer used
+     */
+    id: number
+    /**
+     * The key of the color (i.e. 'berry_red')
+     */
+    key: string
+    /**
+     * The display name of the color (i.e. 'Berry Red')
+     */
+    displayName: string
+    /**
+     * @deprecated Use {@link Color.displayName} instead
+     */
+    name: string
+    /**
+     * The hex value of the color (i.e. '#b8255f')
+     */
+    hexValue: string
+    /**
+     * @deprecated Use {@link Color.hexValue} instead
+     */
+    value: string
+}
+
+export type QuickAddTaskResponse = {
+    id: string
+    projectId: string
+    content: string
+    description: string
+    priority: number
+    sectionId: string | null
+    parentId: string | null
+    childOrder: number // order
+    labels: string[]
+    responsibleUid: string | null
+    checked: boolean // completed
+    addedAt: string // created
+    addedByUid: string | null
+    due: {
+        date: string
+        timezone: string | null
+        isRecurring: boolean
+        string: string
+        lang: string
+    } | null
 }
